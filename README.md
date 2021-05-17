431d2f00bb44c722ae423230c0dad95ead9ae307
Loukas Lampidis <loukas.lampidis@gmail.com>
2021-05-17 22:25:12 +0300

    change the style of the webpage

--- README.md
+++
@@ -1,6 +0,0 @@
-# ProjectWebDev
-# This is a project for the Web Development class of University of Patras.
-This projected was developed by Gryparis Jason and Lampidis Loukas.
-
-
-All code is avaiable for use.
--- js/openingsConstructor.js
+++ js/openingsConstructor.js
@@ -4,7 +4,6 @@ const openings_div=document.querySelector(".open_end_show");
 for (var i = 0; i < 3; i++) {
     let div1=document.createElement("div");
     div1.innerHTML = '<img src="source/chessboard.png" alt="">';
-    //div1.innerHTML = '<p> kaloooooo <';
     let paragraph = document.createElement("p");
     paragraph.innerHTML = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus praesentium similique in voluptas nemo aut eveniet hic optio alias expedita voluptatum ipsum sunt illum, possimus nostrum accusantium, tenetur qui placeat';
     div1.appendChild(paragraph);
